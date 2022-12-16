15702cd7d56bc47d96e25d7d5940e9eb4584c7b8
Jerome Swannack <jeromeswannack@gmail.com>
2022-12-17 12:34:07 +1300

    DataLoaders section of quickstart

--- quickstart.py
+++ quickstart.py
@@ -13,11 +13,25 @@ training_data = datasets.FashionMNIST(
 )
 
 # Download training data from open datasets
-training_data = datasets.FashionMNIST(
+test_data = datasets.FashionMNIST(
     root="fashion_data",
     train=False,
     download=True,
     transform=ToTensor(),
 )
+batch_size = 64
 
+# Create data loaders
+train_dataloader = DataLoader(training_data, batch_size=batch_size)
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+for X, y in test_dataloader:
+    print(f"Shape of X [N, C, H, W]: {X.shape}")
+    print(f"Shape of y: {y.shape} {y.dtype}")
+    break
+
+# Get cpu or gpu device for training
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f" Using {device} device")
 
