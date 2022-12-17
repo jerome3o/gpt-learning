d10c4754bb97d34a4868867af418c62fea2c83f9
Jerome Swannack <jeromeswannack@gmail.com>
2022-12-17 17:25:19 +1300

    Added main block on quickstart

--- quickstart.py
+++ quickstart.py
@@ -49,7 +49,7 @@ def train(
             print(f"loss {loss:>7f} [{current:>5d}/{size:>5d}]")
 
 
-def test(dataloader: DataLoader, model, loss_fn):
+def test(dataloader: DataLoader, model, loss_fn, device):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     model.eval()
@@ -64,79 +64,85 @@ def test(dataloader: DataLoader, model, loss_fn):
         correct /= size
         print(f"Test Error: \n Accuracy: {100*correct:>0.1f}%, Avg loss = {test_loss:>8f} \n")
 
+def main():
+    # Download training data from open datasets
+    training_data = datasets.FashionMNIST(
+        root="fashion_data",
+        train=True,
+        download=True,
+        transform=ToTensor(),
+    )
 
-# Download training data from open datasets
-training_data = datasets.FashionMNIST(
-    root="fashion_data",
-    train=True,
-    download=True,
-    transform=ToTensor(),
-)
+    # Download training data from open datasets
+    test_data = datasets.FashionMNIST(
+        root="fashion_data",
+        train=False,
+        download=True,
+        transform=ToTensor(),
+    )
+    batch_size = 64
 
-# Download training data from open datasets
-test_data = datasets.FashionMNIST(
-    root="fashion_data",
-    train=False,
-    download=True,
-    transform=ToTensor(),
-)
-batch_size = 64
+    # Create data loaders
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
 
-# Create data loaders
-train_dataloader = DataLoader(training_data, batch_size=batch_size)
-test_dataloader = DataLoader(test_data, batch_size=batch_size)
+    for X, y in test_dataloader:
+        print(f"Shape of X [N, C, H, W]: {X.shape}")
+        print(f"Shape of y: {y.shape} {y.dtype}")
+        break
 
-for X, y in test_dataloader:
-    print(f"Shape of X [N, C, H, W]: {X.shape}")
-    print(f"Shape of y: {y.shape} {y.dtype}")
-    break
+    # Get cpu or gpu device for training
 
-# Get cpu or gpu device for training
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using {device} device")
-
-
-model = MyNeuralNetwork().to(device)
-
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-
-epochs = 1
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train(train_dataloader, model, loss_fn, optimizer, device)
-    test(test_dataloader, model, loss_fn)
-print("Done!")
-
-model_path = "model.pth"
-torch.save(model.state_dict(), model_path) 
-print(f"Saved PyTorch Model State to {model_path}")
-
-# loading model
-loaded_model = MyNeuralNetwork()
-loaded_model.load_state_dict(torch.load(model_path))
-
-print(loaded_model)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Using {device} device")
 
 
-classes = [
-    "T-shirt/top",
-    "Trouser",
-    "Pullover",
-    "Dress",
-    "Coat",
-    "Sandal",
-    "Shirt",
-    "Sneaker",
-    "Bag",
-    "Ankle boot",
-]
+    model = MyNeuralNetwork().to(device)
 
-model.eval()
-x, y = test_data[0][0], test_data[0][1]
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
-with torch.no_grad():
-    pred = loaded_model(x)
-    predicted, actual = classes[pred[0].argmax(0)], classes[y]
-    print(f"predicted: '{predicted}', Actual: '{actual}'")
+    epochs = 1
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer, device)
+        test(test_dataloader, model, loss_fn, device)
+    print("Done!")
+
+    model_path = "model.pth"
+    torch.save(model.state_dict(), model_path) 
+    print(f"Saved PyTorch Model State to {model_path}")
+
+    # loading model
+    loaded_model = MyNeuralNetwork()
+    loaded_model.load_state_dict(torch.load(model_path))
+
+    print(loaded_model)
+
+
+    classes = [
+        "T-shirt/top",
+        "Trouser",
+        "Pullover",
+        "Dress",
+        "Coat",
+        "Sandal",
+        "Shirt",
+        "Sneaker",
+        "Bag",
+        "Ankle boot",
+    ]
+
+    model.eval()
+    x, y = test_data[0][0], test_data[0][1]
+
+    with torch.no_grad():
+        pred = loaded_model(x)
+        predicted, actual = classes[pred[0].argmax(0)], classes[y]
+        print(f"predicted: '{predicted}', Actual: '{actual}'")
+
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    main()
