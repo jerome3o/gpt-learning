b22b2ebac32225440e1c22801f8d170eaa61548c
Jerome Swannack <jeromeswannack@gmail.com>
2022-12-17 16:16:43 +1300

    Added train function

--- quickstart.py
+++ quickstart.py
@@ -4,6 +4,51 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
+# Define model
+class MyNeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
+    
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+def train(
+    dataloader: DataLoader,
+    model: MyNeuralNetwork, 
+    loss_fn, 
+    optimizer, 
+    device,
+):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+
 # Download training data from open datasets
 training_data = datasets.FashionMNIST(
     root="fashion_data",
@@ -33,5 +78,10 @@ for X, y in test_dataloader:
 # Get cpu or gpu device for training
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f" Using {device} device")
+print(f"Using {device} device")
 
+
+model = MyNeuralNetwork().to(device)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
