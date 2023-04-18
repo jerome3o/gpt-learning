bec3036597684e3cb09c9d1fc875447ce5389155
Jerome Swannack <jeromeswannack@gmail.com>
2023-04-18 19:37:36 +0000

    Added cache dir

--- llms/1_dolly/load_transformers.py
+++ llms/1_dolly/load_transformers.py
@@ -1,18 +1,36 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+_CACHE_DIR = "/media/raid2/transformers_cache"
+
 
 def main():
-    tokenizer_3b = AutoTokenizer.from_pretrained("databricks/dolly-v2-3b")
-    model_3b = AutoModelForCausalLM.from_pretrained("databricks/dolly-v2-3b")
+    tokenizer_3b = AutoTokenizer.from_pretrained(
+        "databricks/dolly-v2-3b", cache_dir=_CACHE_DIR
+    )
+    model_3b = AutoModelForCausalLM.from_pretrained(
+        "databricks/dolly-v2-3b", cache_dir=_CACHE_DIR
+    )
 
-    tokenizer_6b = AutoTokenizer.from_pretrained("databricks/dolly-v1-6b")
-    model_6b = AutoModelForCausalLM.from_pretrained("databricks/dolly-v1-6b")
+    tokenizer_6b = AutoTokenizer.from_pretrained(
+        "databricks/dolly-v1-6b", cache_dir=_CACHE_DIR
+    )
+    model_6b = AutoModelForCausalLM.from_pretrained(
+        "databricks/dolly-v1-6b", cache_dir=_CACHE_DIR
+    )
 
-    tokenizer_7b = AutoTokenizer.from_pretrained("databricks/dolly-v2-7b")
-    model_7b = AutoModelForCausalLM.from_pretrained("databricks/dolly-v2-7b")
+    tokenizer_7b = AutoTokenizer.from_pretrained(
+        "databricks/dolly-v2-7b", cache_dir=_CACHE_DIR
+    )
+    model_7b = AutoModelForCausalLM.from_pretrained(
+        "databricks/dolly-v2-7b", cache_dir=_CACHE_DIR
+    )
 
-    tokenizer_12b = AutoTokenizer.from_pretrained("databricks/dolly-v2-12b")
-    model_12b = AutoModelForCausalLM.from_pretrained("databricks/dolly-v2-12b")
+    tokenizer_12b = AutoTokenizer.from_pretrained(
+        "databricks/dolly-v2-12b", cache_dir=_CACHE_DIR
+    )
+    model_12b = AutoModelForCausalLM.from_pretrained(
+        "databricks/dolly-v2-12b", cache_dir=_CACHE_DIR
+    )
 
 
 if __name__ == "__main__":
