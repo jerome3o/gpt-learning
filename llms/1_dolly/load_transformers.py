fb64a305a25f1a542894d017c4eecc48ba717851
Jerome Swannack <jeromeswannack@gmail.com>
2023-04-19 06:54:16 +0000

    Loaded in 3b, running on cpu

--- llms/1_dolly/load_transformers.py
+++ llms/1_dolly/load_transformers.py
@@ -1,15 +1,22 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from instruct_pipeline import InstructionTextGenerationPipeline
 
 _CACHE_DIR = "/mnt/raid2/transformers_cache"
 
 
 def main():
-    # tokenizer_3b = AutoTokenizer.from_pretrained(
-    #     "databricks/dolly-v2-3b", cache_dir=_CACHE_DIR
-    # )
-    # model_3b = AutoModelForCausalLM.from_pretrained(
-    #     "databricks/dolly-v2-3b", cache_dir=_CACHE_DIR
-    # )
+    tokenizer_3b = AutoTokenizer.from_pretrained(
+        "databricks/dolly-v2-3b",
+        cache_dir=_CACHE_DIR,
+        device_map="cpu",
+        # load_in_8bit_fp32_cpu_offload
+    )
+    model_3b = AutoModelForCausalLM.from_pretrained(
+        "databricks/dolly-v2-3b",
+        cache_dir=_CACHE_DIR,
+        device_map="auto",
+        # load_in_8bit=True,
+    )
 
     # tokenizer_6b = AutoTokenizer.from_pretrained(
     #     "databricks/dolly-v1-6b", cache_dir=_CACHE_DIR
@@ -25,12 +32,25 @@ def main():
     #     "databricks/dolly-v2-7b", cache_dir=_CACHE_DIR
     # )
 
-    tokenizer_12b = AutoTokenizer.from_pretrained(
-        "databricks/dolly-v2-12b", cache_dir=_CACHE_DIR
-    )
-    model_12b = AutoModelForCausalLM.from_pretrained(
-        "databricks/dolly-v2-12b", cache_dir=_CACHE_DIR
-    )
+    # tokenizer_12b = AutoTokenizer.from_pretrained(
+    #     "databricks/dolly-v2-12b",
+    #     cache_dir=_CACHE_DIR,
+    #     device_map="auto",
+    # )
+    # model_12b = AutoModelForCausalLM.from_pretrained(
+    #     "databricks/dolly-v2-12b",
+    #     cache_dir=_CACHE_DIR,
+    #     device_map="auto",
+    #     load_in_8bit=True,
+    # )
+
+    print(model_3b)
+
+    model = model_3b
+    tokenizer = tokenizer_3b
+
+    generate_text = InstructionTextGenerationPipeline(model=model, tokenizer=tokenizer)
+    res = generate_text("Write me a poem about frogs")
 
 
 if __name__ == "__main__":
