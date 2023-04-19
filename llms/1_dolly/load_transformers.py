96014d61268b2a9937d65b63d425e8efcef318ad
Jerome Swannack <jeromeswannack@gmail.com>
2023-04-19 19:32:49 +0000

    Working on getting bitsandbytes working for ROCm

--- llms/1_dolly/load_transformers.py
+++ llms/1_dolly/load_transformers.py
@@ -8,14 +8,13 @@ def main():
     tokenizer_3b = AutoTokenizer.from_pretrained(
         "databricks/dolly-v2-3b",
         cache_dir=_CACHE_DIR,
-        device_map="cpu",
-        # load_in_8bit_fp32_cpu_offload
+        device_map="auto",
     )
     model_3b = AutoModelForCausalLM.from_pretrained(
         "databricks/dolly-v2-3b",
         cache_dir=_CACHE_DIR,
         device_map="auto",
-        # load_in_8bit=True,
+        load_in_8bit=True,
     )
 
     # tokenizer_6b = AutoTokenizer.from_pretrained(
---
+++ llms/quantization/README.md
@@ -0,0 +1,6 @@
+# Getting LLM.int8() Quantisation working on AMD GPUs
+
+* Need to get [bitsandbytes](https://github.com/TimDettmers/bitsandbytes) package working with ROCm
+* bronctoc's rocm fork [here](https://github.com/broncotc/bitsandbytes-rocm)
+    * No docs on how to get it to build
+* jinsihou19's fork has a make command for hip [here](https://github.com/jinsihou19/bitsandbytes-rocm)
