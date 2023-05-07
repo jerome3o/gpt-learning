b4276a98129f1a58788465ae2fdd71b8985a1622
Jerome Swannack <jeromeswannack@gmail.com>
2023-05-07 22:55:23 +1200

    Added preprocessing functions

--- tut/causal_lm/1_causal_lm.py
+++ tut/causal_lm/1_causal_lm.py
@@ -6,12 +6,45 @@ from datasets import load_dataset
 from transformers import AutoTokenizer
 
 
+def preprocess_function(examples, tokenizer):
+    return tokenizer([" ".join(x) for x in examples["answers.text"]])
+
+
+block_size = 128
+
+
+def group_texts(examples):
+    # Concatenate all texts.
+    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    # We drop the small remainder, we could add padding if the model supported
+    # it instead of this drop, you can customize this part to your needs.
+    if total_length >= block_size:
+        total_length = (total_length // block_size) * block_size
+    # Split by chunks of block_size.
+    result = {
+        k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+        for k, t in concatenated_examples.items()
+    }
+    result["labels"] = result["input_ids"].copy()
+    return result
+
+
 def main():
     eli5 = load_dataset("eli5", split="train_asks[:5000]")
     eli5 = eli5.train_test_split(test_size=0.2)
-    print(eli5["train"][0])
+    eli5 = eli5.flatten()
 
     tokensizer = AutoTokenizer.from_pretrained("distilgpt2")
+
+    tokenized_eli5 = eli5.map(
+        lambda x: preprocess_function(x, tokenizer=tokensizer),
+        batched=True,
+        num_proc=4,
+        remove_columns=eli5["train"].column_names,
+    )
+    lm_dataset = tokenized_eli5.map(group_texts, batched=True, num_proc=4)
+    print(lm_dataset)
 
 
 if __name__ == "__main__":
