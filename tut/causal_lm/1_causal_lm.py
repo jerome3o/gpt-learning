ec188f9117e5fca42b2e300ebd7ee4f892ca9405
Jerome Swannack <jeromeswannack@gmail.com>
2023-05-09 08:03:21 +1200

    Added training section to causal lm tut

--- tut/causal_lm/1_causal_lm.py
+++ tut/causal_lm/1_causal_lm.py
@@ -3,7 +3,13 @@
 
 # pip install transformers datasets evaluate
 from datasets import load_dataset
-from transformers import AutoTokenizer
+from transformers import (
+    AutoTokenizer,
+    DataCollatorForLanguageModeling,
+    AutoModelForCausalLM,
+    TrainingArguments,
+    Trainer,
+)
 
 
 def preprocess_function(examples, tokenizer):
@@ -35,16 +41,38 @@ def main():
     eli5 = eli5.train_test_split(test_size=0.2)
     eli5 = eli5.flatten()
 
-    tokensizer = AutoTokenizer.from_pretrained("distilgpt2")
+    tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
 
     tokenized_eli5 = eli5.map(
-        lambda x: preprocess_function(x, tokenizer=tokensizer),
+        lambda x: preprocess_function(x, tokenizer=tokenizer),
         batched=True,
         num_proc=4,
         remove_columns=eli5["train"].column_names,
     )
     lm_dataset = tokenized_eli5.map(group_texts, batched=True, num_proc=4)
-    print(lm_dataset)
+
+    tokenizer.pad_token = tokenizer.eos_token
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+    model = AutoModelForCausalLM.from_pretrained("distilgpt2")
+
+    training_args = TrainingArguments(
+        output_dir="models",
+        evaluation_strategy="epoch",
+        learning_rate=2e-5,
+        weight_decay=0.01,
+        push_to_hub=False,
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=lm_dataset["train"],
+        eval_dataset=lm_dataset["test"],
+        data_collator=data_collator,
+    )
+
+    trainer.train()
 
 
 if __name__ == "__main__":
