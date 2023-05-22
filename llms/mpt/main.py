40b17f2a2b858976439c31a43856d5417f85cd61
Jerome Swannack <jeromeswannack@gmail.com>
2023-05-22 04:15:28 +0000

    Tinkering with mpt

--- llms/mpt/main.py
+++ llms/mpt/main.py
@@ -1,4 +1,5 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
+import torch
 
 from llms.model_loaders import DEFAULT_CACHE_DIR
 
@@ -11,14 +12,52 @@ def main():
         model_name,
         cache_dir=cache_dir,
     )
-    model = AutoModelForCausalLM.from_pretrained(
+    config = AutoConfig.from_pretrained(
         model_name,
         trust_remote_code=True,
         revision="671f67f",
         cache_dir=cache_dir,
     )
+    config.attn_config["attn_impl"] = "flash"
+    config.attn_config["alibi"] = False
 
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        config=config,
+        trust_remote_code=True,
+        torch_dtype=torch.float16,
+        revision="671f67f",
+        cache_dir=cache_dir,
+    )
     print(model, tokenizer)
+
+    INSTRUCTION_KEY = "### Instruction:"
+    RESPONSE_KEY = "### Response:"
+    INTRO_BLURB = "Below is an instruction that describes a task. Write a response that appropriately completes the request."
+    PROMPT_FOR_GENERATION_FORMAT = """{intro}
+    {instruction_key}
+    {instruction}
+    {response_key}
+    """.format(
+        intro=INTRO_BLURB,
+        instruction_key=INSTRUCTION_KEY,
+        instruction="{instruction}",
+        response_key=RESPONSE_KEY,
+    )
+
+    example = "Please write a poem about frogs."
+    fmt_ex = PROMPT_FOR_GENERATION_FORMAT.format(instruction=example)
+
+    inputs = tokenizer([fmt_ex], return_tensors="pt").to("cuda")
+    outputs = model.generate(
+        **inputs,
+        temperature=0.7,
+        max_new_tokens=1000,
+        pad_token_id=tokenizer.eos_token_id,
+        do_sample=True,
+    )
+    text = tokenizer.decode(outputs[0])
+    print(text)
 
 
 if __name__ == "__main__":
