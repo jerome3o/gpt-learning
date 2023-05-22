e4d9dacbc9e795ec938a4835b8fdaaf7e5cf0134
Jerome Swannack <jeromeswannack@gmail.com>
2023-05-22 02:41:19 +0000

    Getting the model downloading

--- llms/mpt/main.py
+++ llms/mpt/main.py
@@ -1,10 +1,25 @@
-from transformers import AutoModelForCausalLM
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from llms.model_loaders import DEFAULT_CACHE_DIR
 
 
 def main():
-    model = AutoModelForCausalLM.from_pretrained("mosaicml/mpt-7b-instruct")
+    model_name = "mosaicml/mpt-7b-instruct"
+
+    cache_dir = DEFAULT_CACHE_DIR
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_name,
+        device_map="auto",
+        cache_dir=cache_dir,
+    )
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        device_map="auto",
+        trust_remote_code=True,
+        cache_dir=cache_dir,
+    )
+
+    print(model, tokenizer)
 
 
 if __name__ == "__main__":
