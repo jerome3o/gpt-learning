b032b161e41b65cee0f4451777e3f8f06c85bdd8
Jerome Swannack <jeromeswannack@gmail.com>
2023-05-22 04:18:50 +0000

    Added resnet script + config

---
+++ inference/amdinference/get_model.sh
@@ -0,0 +1,3 @@
+wget https://github.com/onnx/models/raw/main/vision/classification/resnet/model/resnet50-v2-7.onnx
+mkdir -p ./model_repository/resnet50/1
+mv ./resnet50-v2-7.onnx ./model_repository/resnet50/1/
