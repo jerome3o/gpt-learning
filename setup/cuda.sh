4077550bd813a619ced7432de2770bb749ccb40d
Jerome Swannack <jeromeswannack@gmail.com>
2023-05-27 08:08:13 +0000

    Added rest of cuda install

--- setup/cuda.sh
+++ setup/cuda.sh
@@ -8,3 +8,15 @@ uname -r
 
 
 sudo apt-key del 7fa2af80
+
+
+# install keyring?
+distro=ubuntu2204
+arch=x86_64
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
+
+# Install cuda
+sudo apt-get install cuda
+
