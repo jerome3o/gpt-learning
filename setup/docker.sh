8922f65e47dec0d20f804c193756aad8e26fc538
Jerome Swannack <jeromeswannack@gmail.com>
2023-05-27 08:24:14 +0000

    Docker setup script

---
+++ setup/docker.sh
@@ -0,0 +1,20 @@
+set -xe
+
+# Update apt
+sudo apt-get update
+sudo apt-get -y install ca-certificates curl gnupg
+
+# Add gpg key
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Setup docker apt repository
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# install docker engine
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
