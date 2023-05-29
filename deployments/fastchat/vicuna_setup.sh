1b7ceeacc93125fe42633f9ac2fb012a4ec4945d
Jerome Swannack <jeromeswannack@gmail.com>
2023-05-29 05:18:18 +0000

    Added vicuna setup script

--- deployments/fastchat/vicuna_setup.sh
+++ deployments/fastchat/vicuna_setup.sh
@@ -1,8 +1,23 @@
 set -xe
 
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python \
+  python -m transformers.models.llama.convert_llama_weights_to_hf \
+  --input_dir /mnt/raid/llama/llama-dl/ \
+  --output_dir /mnt/raid/llama/hf/7B/ \
+  --model_size 7B
+
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python \
+  python -m transformers.models.llama.convert_llama_weights_to_hf \
+  --input_dir /mnt/raid/llama/llama-dl/ \
+  --output_dir /mnt/raid/llama/hf/13B/ \
+  --model_size 13B
 
 python -m fastchat.model.apply_delta \
-    --base-model-path /mnt/raid/llama/llama-dl/13B \
-    --target-model-path /mnt/raid/vicuna/13B \
-    --delta-path lmsys/vicuna-13b-delta-v1.1
+    --base-model-path "/mnt/raid/llama/hf/7B/" \
+    --target-model-path "/mnt/raid/vicuna/7B/" \
+    --delta-path lmsys/vicuna-7b-delta-v1.1
 
+python -m fastchat.model.apply_delta \
+    --base-model-path "/mnt/raid/llama/hf/13B/" \
+    --target-model-path "/mnt/raid/vicuna/13B/" \
+    --delta-path lmsys/vicuna-13b-delta-v1.1
