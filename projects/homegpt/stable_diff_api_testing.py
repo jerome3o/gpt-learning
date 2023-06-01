61315f93856dbe774d5b22888d1a6bfa56453e6f
Jerome Swannack <jeromeswannack@gmail.com>
2023-06-01 04:47:26 +0000

    Tidied up stable_diff_api_testing

--- projects/homegpt/stable_diff_api_testing.py
+++ projects/homegpt/stable_diff_api_testing.py
@@ -1,21 +1,24 @@
 import os
+from pathlib import Path
+
 import webuiapi
 
 HOST = os.environ.get("STABLE_DIFF_API_HOST")
 PORT = os.environ.get("STABLE_DIFF_API_PORT")
 
 
-def main():
-    api = webuiapi.WebUIApi(host=HOST, port=PORT, steps=50)
-    result = api.txt2img(
-        prompt="a big hairy horse eating ramen noodles from a trough", 
-        negative_prompt="noodles are bad for horses",
-    )
-    print(result.image)
-    # result.image is a PIL.Image
+_output_dir = Path("images/")
 
+def main():
+    api = webuiapi.WebUIApi(host=HOST, port=PORT, steps=20)
+    result = api.txt2img(
+        prompt="A frog wizard holding a wand", 
+    )
+
+    # result.image is a PIL.image
     # Save to file
-    result.image.save("images/horse.png")
+    _output_dir.mkdir(parents=True, exist_ok=True)
+    result.image.save(_output_dir / "horse.png")
 
 
 if __name__ == "__main__":
